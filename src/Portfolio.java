9192753945961f7a3681d35407c1f9205c07cfae
Quang Vu <qdv2130@rit.edu>
2015-10-11 19:37:40 -0400

    Add the state of reading Transaction from porfolio file

--- src/Portfolio.java
+++ src/Portfolio.java
@@ -6,9 +6,11 @@ public class Portfolio extends Observable {
 	private int userID;
 	private ArrayList<Account> allAccount;
 	private HashMap<String, Integer> holding;
+	private ArrayList<Transaction> allTransaction;
 	
-	public Portfolio(ArrayList<Account> allAccount, HashMap<String, Integer> holding){
+	public Portfolio(ArrayList<Account> allAccount, HashMap<String, Integer> holding, ArrayList<Transaction> allTransaction){
 		this.allAccount = allAccount;
 		this.holding = holding;
+		this.allTransaction = allTransaction;
 	}
 }
--- src/PortfolioParser.java
+++ src/PortfolioParser.java
@@ -3,9 +3,11 @@ import java.io.*;
 
 public class PortfolioParser {
 	
-	public void importFile(String filename){
+	public Portfolio importFile(String filename){
 		String line = null;
+		Portfolio portfolio = null;
 		ArrayList<Account> allAccount = new ArrayList<Account>();
+		ArrayList<Transaction> allTransaction = new ArrayList<Transaction>();
 		HashMap<String, Integer> holding = new HashMap<String, Integer>();
 		try{
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
@@ -26,9 +28,19 @@ public class PortfolioParser {
 					int value = Integer.valueOf(separateValue[2]);
 					holding.put(separateValue[1], value);
 				}else{
-					
+					if(separateValue[1].equals("S")){
+						sellTransaction sell = new sellTransaction(separateValue[2], separateValue[3], Float.valueOf(separateValue[4]), Integer.valueOf(separateValue[5]));
+						allTransaction.add(sell);
+					}else if(separateValue[1].equals("B")){
+						transferTransaction transfer = new transferTransaction(separateValue[2], separateValue[3], Float.valueOf(separateValue[4]));
+						allTransaction.add(transfer);
+					}else{
+						buyTransaction buy = new buyTransaction(separateValue[2], separateValue[3], Float.valueOf(separateValue[4]), Integer.valueOf(separateValue[5]));
+						allTransaction.add(buy);
+					}
 				}
 			}
+			portfolio = new Portfolio(allAccount, holding, allTransaction);
 			bufferedReader.close();
 		}
 		catch(FileNotFoundException e){
@@ -37,5 +49,6 @@ public class PortfolioParser {
 		catch(IOException e){
 			e.printStackTrace();
 		}
+		return portfolio;
 	}
 }
