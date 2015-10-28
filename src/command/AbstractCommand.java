a9fbb2c593cca4938c985ed65ac81faba71c4d2b
Mitchell Derby <mxd5415@rit.edu>
2015-10-28 15:52:55 -0400

    Added Command Package

---
+++ src/command/AbstractCommand.java
@@ -0,0 +1,9 @@
+package command;
+
+public abstract class AbstractCommand {
+
+	abstract void execute();
+	
+	abstract void unexecute();
+	
+}
---
+++ src/command/BuyCommand.java
@@ -0,0 +1,30 @@
+package command;
+
+
+public class BuyCommand extends AbstractCommand {
+
+	String ticker;
+	Float price;
+	int NumShares;
+	Account account;
+	
+	public BuyCommand(String ticker, Float price, int NumShares, Account account){
+		this.account = account;
+		this.NumShares = NumShares;
+		this.price = price;
+		this.ticker = ticker;
+	}
+	
+	@Override
+	void execute() {
+		
+
+	}
+
+	@Override
+	void unexecute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
