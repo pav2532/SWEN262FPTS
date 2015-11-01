d0f2edbcefccecc6423f6b5b092960f39eb81838
Quang Vu <qdv2130@rit.edu>
2015-11-01 17:07:03 -0500

    Added DowJones calculation

--- src/DowJones.java
+++ src/DowJones.java
@@ -1,9 +1,16 @@
 /**
  * Created by Quang on 10/31/15.
  */
+import java.util.*;
+
 public class DowJones {
+    private int sum;
+    private double divisor = 0.149;
 
-    public void caculation(Float divisor){
-
+    public double caculation(HashMap<String, Integer> thirtyStock){
+        for(String key : thirtyStock.keySet()){
+            sum += thirtyStock.get(key);
+        }
+        return (sum/divisor);
     }
 }
